63960a9553de1400ea174e3a23b87ad619452dc3
marwan <marwanackerman@gmail.com>
2025-06-04 11:39:58 +0200

    Update acceuilControleur.php

--- src/controleur/acceuilControleur.php
+++ src/controleur/acceuilControleur.php
@@ -3,6 +3,10 @@ function acceuilControleur($twig){
     echo $twig->render('acceuil.html.twig', array());
 }
 
+function contactControleurs($twig,$db) { // pas encore de page contact 
+    echo $twig->render('contact.html.twig');
+}
+
 function maintenanceControleur($twig,$db) {
     echo $twig->render('maintenance.html.twig',array());
 }
@@ -14,4 +18,5 @@ function aproposControleur($twig,$db) {
 function mentionsLegalesControleurs($twig,$db) {
     echo $twig->render('mentionsLegales.html.twig');
 }
+
 ?>
