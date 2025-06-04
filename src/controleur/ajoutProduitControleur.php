bd019baf369e5d911060259be1a3bacb55e64302
marwan <marwanackerman@gmail.com>
2025-06-04 11:40:37 +0200

    Update ajoutProduitControleur.php

--- src/controleur/ajoutProduitControleur.php
+++ src/controleur/ajoutProduitControleur.php
@@ -2,7 +2,7 @@
 function ajoutProduitControleur($twig, $db) {
     $jeuManager = new Jeu($db);
 
-    // Ajouter
+    // Ajouter un jeu + securité des champ
     if (isset($_POST['btAjouter'])) {
         $titre = htmlspecialchars($_POST['titre']);
         $plateforme = htmlspecialchars($_POST['plateforme']);
@@ -16,23 +16,26 @@ function ajoutProduitControleur($twig, $db) {
 
     $liste = $jeuManager->selectAll();
 
+    //ajouter  
     if(isset($_POST['btAjouter'])){
- $photo =null;
- $produit = new Produit($db);
- $designation = $_POST['inputDesignation'];
- $description = $_POST['inputDescription'];
- $prix = $_POST['inputPrix'];
- $idType = $_POST['inputType'];
- $upload = new Upload(array('png', 'gif', 'jpg', 'jpeg'), 'photo', 500000);
- $photo = $upload->enregistrer('photo');
- $exec=$produit->insert($designation, $description, $prix, $idType, $photo['nom']);
- if (!$exec){
- $form['valide'] = false;
- $form['message'] = 'Problème d\'insertion dans la table produit ';
- }else{
- $form['valide'] = true;
- }
- }
+        $photo =null;
+        $produit = new Produit($db);
+        $designation = $_POST['inputDesignation'];
+        $description = $_POST['inputDescription'];
+        $prix = $_POST['inputPrix'];
+        $idType = $_POST['inputType'];
+        $upload = new Upload(array('png', 'gif', 'jpg', 'jpeg'), 'photo', 500000);
+        $photo = $upload->enregistrer('photo');
+        $exec=$produit->insert($designation, $description, $prix, $idType, $photo['nom']);
+
+      if (!$exec){
+
+        $form['valide'] = false;
+        $form['message'] = 'Problème d\'insertion dans la table produit ';
+        }else{
+        $form['valide'] = true;
+        }
+        }
 
 
     echo $twig->render('ajoutProduit.html.twig', ['liste' => $liste]);
