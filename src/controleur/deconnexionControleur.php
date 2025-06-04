cd2b1bc3460eba20cf716b075d894db81fa12e7c
marwan <marwanackerman@gmail.com>
2025-06-04 11:41:45 +0200

    Update deconnexionControleur.php

--- src/controleur/deconnexionControleur.php
+++ src/controleur/deconnexionControleur.php
@@ -1,6 +1,6 @@
 <?php
 function deconnexionControleur($twig, $db){
-    session_unset();
+    session_unset(); // destruction de la seesion apres deconnexion (secruiter)
     session_destroy();
     header("Location:index.php");
 }
