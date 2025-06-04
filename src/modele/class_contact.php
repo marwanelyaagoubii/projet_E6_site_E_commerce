a49f2f13405d62d9fc477753760d6bcaca6f24f8
marwan <marwanackerman@gmail.com>
2025-06-04 11:53:00 +0200

    Update class_contact.php

--- src/modele/class_contact.php
+++ src/modele/class_contact.php
@@ -1,25 +1,6 @@
 <?php
 
 class Contact {
-    private $db;
-    private $insert;
-
-    public function __construct($db) {
-        $this->db = $db;
-        $this->insert = $this->db->prepare(
-            "INSERT INTO contact (name, email, message)
-             VALUES (:name, :email, :message)"
-        );
-    }
-
-    public function insert($name, $email, $message) {
-        $this->insert->bindValue(':name', $name);
-        $this->insert->bindValue(':email', $email);
-        $this->insert->bindValue(':message', $message);
-
-        $result = $this->insert->execute();
-
-        return $result;
-    }
+// pour crée le lien du formulire contact a la bdd 
 }
 ?>
